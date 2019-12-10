5c82ebbb1877454516f76971da9806a3d8d89e45
Christina B <christinab@aycmedia.com>
2019-12-10 07:31:15 -0500

    wip more generic script

--- app.js
+++ app.js
@@ -1,52 +1,125 @@
-pElements = document.getElementsByTagName("p");
+window.addEventListener("mouseup", function(evt) {
+    //alert("mouseup");
+    var targ = evt.target;
 
-for (var i = 0; i < pElements.length; i++) {
-    pElements[i].onclick = function getPostDetailsOnClick() {
-        var message = "";
-        var user = "";
-        var user_link = "";
-        var link = "";
+    // Set the page URL if not set
+    if (content["page_link"].length == 0) {
+        content["page_link"] = window.location.href;
+        updateFrontEndDisplay("page_link");
+    }
 
-        console.log("getPostDetailsOnClick");
-        console.log(this);
-
-        // first parent div out *should* contain list of <p>
-        var parentDiv1 = this.parentElement;
-        var allPsInThisDiv = parentDiv1.getElementsByTagName("p");
-        console.log(allPsInThisDiv);
-        for (var pI = 0; pI < allPsInThisDiv.length; pI++) {
-            var p = allPsInThisDiv[pI];
-            message += "<p>" + p.innerText + "</p>";
+    if (current_action.length > 0) {
+        if (current_action == "title" || current_action == "author") {
+            // get the selected text
+            var selectedText = getSelectedText();
+            content[current_action] = selectedText;
+        } else if (current_action == "content") {
+            var selectedText = getHTMLOfSelection();
+            content[current_action] = selectedText;
+        } else if (current_action == "link") {
+            // get link from highlight
+            var selectedText = getLinkFromSelection();
+            content[current_action] = selectedText;
         }
-        console.log(message);
 
-        // now we need to extract the user and link
-        var parentDiv2 = parentDiv1.parentElement;
-        var parentDiv3 = parentDiv2.parentElement;
-        console.log(parentDiv3);
+        updateFrontEndDisplay(current_action);
+    }
+});
 
-        var parentDiv3ATags = parentDiv3.getElementsByTagName("a");
-        console.log(parentDiv3ATags);
-        // now look into the divs
-        var hasUser = false;
-        var hasLink = false;
-        for (var aI = 0; aI < parentDiv3ATags.length; aI++) {
-            if (!hasUser && !hasLink) {
-                var a = parentDiv3ATags[aI];
-                var href = a.getAttribute("href");
-                console.log(href);
-                if (href.includes("/user/")) {
-                    console.log("yay we found the user!");
-                    user_link = href;
-                    var user_name = a.innerText;
-                    user = user_name;
-                } else if (href.includes("/comments/")) {
-                    link = href;
-                }
+var content = {};
+content["page_link"] = "";
+content["title"] = "";
+content["content"] = "";
+content["content"] = "";
+content["link"] = "";
+
+var setTitleElement = document.getElementById("settitle");
+console.log(setTitleElement);
+setTitleElement.addEventListener("click",function(e){
+    setAction("title");
+},false);
+
+var current_action = "link";
+var selected_text = "";
+
+function updateFrontEndDisplay(variable) {
+    console.log("updateFrontEndDisplay: " + variable);
+    if (content[variable] === undefined) {
+        return;
+    }
+
+    var element = document.getElementById(variable);
+    if (element !== undefined) {
+        element.value = content[variable];
+    }
+}
+
+function setAction(action) {
+    // Set the current action
+    console.log("Setting capture action to: " + action);
+    current_action = action;
+
+    var element = document.getElementById("current_action");
+    element.innerHTML = action;
+}
+
+function getSelectedText() {
+    var txt = "";
+    if (window.getSelection) {
+        txt = window.getSelection().toString();
+    } else if (document.getSelection) {
+        txt = document.getSelection();
+    } else if (document.selection) {
+        txt = document.selection.createRange().text;
+    }
+    return txt;
+}
+
+function getHTMLOfSelection() {
+    var range;
+    if (document.selection && document.selection.createRange) {
+        range = document.selection.createRange();
+        return range.htmlText;
+    } else if (window.getSelection) {
+        var selection = window.getSelection();
+        if (selection.rangeCount > 0) {
+            range = selection.getRangeAt(0);
+            var clonedSelection = range.cloneContents();
+            var div = document.createElement("div");
+            div.appendChild(clonedSelection);
+            return div.innerHTML;
+        }
+    }
+
+    return "";
+}
+
+function getLinkFromSelection() {
+    console.log("getLinkFromSelection");
+    var range;
+    if (document.selection && document.selection.createRange) {
+        range = document.selection.createRange();
+        return range.htmlText;
+    } else if (window.getSelection) {
+        var selection = window.getSelection();
+        if (selection.rangeCount > 0) {
+            range = selection.getRangeAt(0);
+            var clonedSelection = range.cloneContents();
+            var parentNode = selection.anchorNode.parentElement;
+            var clonedParentNode = parentNode.cloneNode(true);
+            console.log(parentNode);
+            console.log(clonedParentNode);
+            var div = document.createElement("div");
+            div.appendChild(clonedParentNode);
+            var aElements = div.querySelectorAll("a");
+            if (aElements.length > 0) {
+                var aElement = aElements[0];
+                var href = aElement.getAttribute("href");
+                div.removeChild(clonedParentNode);
+                return href;
             }
-
         }
+    }
 
-
-    };
+    return "";
 }
